aba0005a0219abeaa1ff6b81ed0ac651de5cbaad
Evan Tanudjaja <32850711+ElectricEvan@users.noreply.github.com>
2021-06-09 00:00:54 -0400

    Sync gitignore

--- COGS/mod.py
+++ COGS/mod.py
@@ -72,6 +72,8 @@ class Moderation(commands.Cog):
             await msg.channel.send("SURRENDER NOW OR PREPARE TO FIGHT")
         elif "ehe" in msg.content.strip().lower() and not msg.author.bot:
             await msg.reply("\"EHE\", TE NANDAYO?!", mention_author=True)
+        elif "omae was mou shindeiru" in msg.content.stip().lower() and not msg.author.bot:
+            await msg.reply("NANI?!", mention_author=True)
 
 
 def setup(client):
