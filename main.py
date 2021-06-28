222166bef06e3a1460eccedc18a00e1aef177bc4
Evan Tanudjaja <32850711+ElectricEvan@users.noreply.github.com>
2021-06-27 23:55:07 -0400

    Help Command 2.0

--- main.py
+++ main.py
@@ -3,11 +3,29 @@ from discord.ext import commands
 import os
 from dotenv import load_dotenv
 
+
+class CustomHelpCommand(commands.HelpCommand):
+
+    def __init__(self):
+        super().__init__()
+
+    async def send_bot_help(self, mapping):
+        for cog in mapping:
+            await self.get_destination().send(f"{cog.qualified_name}: {[command.name for command in mapping[cog]]}")
+
+    async def send_cog_help(self, cog):
+        await self.get_destination().send(f"{cog.qualified_name}: {[command.name for command in cog.get_commands()]}")
+
+    async def send_group_help(self, group):
+        await self.get_destination().send(f"{group.name}: {[command.name for index, command in enumerate(group.commands)]}")
+
+    async def send_command_help(self, command):
+        await self.get_destination().send(commands.name)
+
+
 load_dotenv(".env")
 intents = discord.Intents.all()
-client = commands.Bot(command_prefix="-", intents=intents)
-client.remove_command("help")
-
+client = commands.Bot(command_prefix="-", intents=intents, help_command=CustomHelpCommand)
 temp_users = {}
 
 
